939156f92cf67e545c96c1043af5e9bdf4ba1e84
Marc Ocampo <mdocampo1@gmail.com>
2020-03-22 12:02:11 +0800

    compress and extract functions

--- custom_bashrc.sh
+++ custom_bashrc.sh
@@ -16,16 +16,13 @@ DBG=1
 ######################################################
 # ALIASES
 ######################################################
-alias xremote="cd ${DIR_MOUNT} && xls"
-alias xhome="cd ${DIR_HOME} && xls"
+alias xls="clear && ls -lrtA"
+alias xremote="xcd ${DIR_MOUNT}"
+alias xhome="xcd ${DIR_HOME}"
 
 ######################################################
 # FUNCTIONS
 ######################################################
-function xls() {
-  clear && ls -lrtA $1
-}
-
 function xcd() {
   if [ $# -lt 2 ]; then
     cd $1
@@ -42,6 +39,50 @@ function xdbgEcho() {
     if [ $DBG -eq 1 ]; then
       echo "[DBG]" $*
     fi
+  fi
+}
+
+function compress {
+  if [ $# -gt 1 ]; then
+    # tar -czvf archive.tar foo bar  # Create archive.tar from files foo and bar
+    tar -czvf $1 ${@:2}
+  fi
+}
+
+# https://github.com/xvoland/Extract/blob/master/extract.sh
+function extract {
+  if [ -z "$1" ]; then
+    # display usage if no parameters given
+    echo "Usage: extract <path/file_name>.<zip|rar|bz2|gz|tar|tbz2|tgz|Z|7z|xz|ex|tar.bz2|tar.gz|tar.xz>"
+    echo "       extract <path/file_name_1.ext> [path/file_name_2.ext] [path/file_name_3.ext]"
+    return 1
+  else
+    for n in $@
+    do
+      if [ -f "$n" ] ; then
+          case "${n%,}" in
+            *.tar.bz2|*.tar.gz|*.tar.xz|*.tbz2|*.tgz|*.txz|*.tar)
+                         tar xvf "$n"       ;;
+            *.lzma)      unlzma ./"$n"      ;;
+            *.bz2)       bunzip2 ./"$n"     ;;
+            *.rar)       unrar x -ad ./"$n" ;;
+            *.gz)        gunzip ./"$n"      ;;
+            *.zip)       unzip ./"$n"       ;;
+            *.z)         uncompress ./"$n"  ;;
+            *.7z|*.arj|*.cab|*.chm|*.deb|*.dmg|*.iso|*.lzh|*.msi|*.rpm|*.udf|*.wim|*.xar)
+                         7z x ./"$n"        ;;
+            *.xz)        unxz ./"$n"        ;;
+            *.exe)       cabextract ./"$n"  ;;
+            *)
+                         echo "extract: '$n' - unknown archive method"
+                         return 1
+                         ;;
+          esac
+      else
+          echo "'$n' - file does not exist"
+          return 1
+      fi
+    done
   fi
 }
 
